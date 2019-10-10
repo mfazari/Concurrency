b87b224bd32f2a2f65d33d73cb532266545fa32f
mfazari <massimofazari@bluewin.ch>
2019-10-10 16:33:27 +0200

    sequential implemented

--- Tasks/assignment3/src/ethz/ch/pp/assignment3/counters/SequentialCounter.java
+++ Tasks/assignment3/src/ethz/ch/pp/assignment3/counters/SequentialCounter.java
@@ -1,6 +1,17 @@
 package ethz.ch.pp.assignment3.counters;
 
 //TODO: implement
-public class SequentialCounter implements Counter {
+    public class SequentialCounter implements Counter {
+        private int c = 0;
+
+        public void increment() {
+            c++;
+        }
+
+        public int value() {
+            return c;
+        }
+    }
+
 
 }
